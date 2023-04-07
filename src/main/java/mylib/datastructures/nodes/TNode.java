9c2eade71954d3994610a057ee500e576d47f2ca
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2023-04-07 00:50:46 -0600

    added some methods from requirements

--- src/main/java/mylib/datastructures/nodes/TNode.java
+++ src/main/java/mylib/datastructures/nodes/TNode.java
@@ -1,15 +1,28 @@
 package mylib.datastructures.nodes;
 
 public class TNode {
-    private int data;             // data stored in the node
+    private int data;          // data stored in the node
     private TNode leftChild;   // reference to the left child node
     private TNode rightChild;  // reference to the right child node
-    
-    // constructor to create a new node with given data
-    public TNode(int data) {
-        this.data = data;
+    private TNode parent;      // reference to the parent node
+    private int balance;       // balance factor of the node
+
+    // default constructor
+    public TNode() {
+        this.data = 0;
         this.leftChild = null;
         this.rightChild = null;
+        this.parent = null;
+        this.balance = 0;
+    }
+
+    // overload constructor
+    public TNode(int data, int balance, TNode parent, TNode leftChild, TNode rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+        this.parent = parent;
+        this.balance = balance;
     }
     
     // getter and setter methods for data
@@ -37,6 +50,34 @@ public class TNode {
     
     public void setRightChild(TNode rightChild) {
         this.rightChild = rightChild;
+    }
+
+    // getter and setter methods for parent node reference
+    public TNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TNode parent) {
+        this.parent = parent;
+    }
+
+    // getter and setter methods for balance factor
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    // print method to print node information in a user-friendly format
+    public void print() {
+        System.out.println("Data: " + data + ", Balance: " + balance);
+    }
+
+    // toString method to return data as a string
+    public String toString() {
+        return Integer.toString(data);
     }
 }
 
