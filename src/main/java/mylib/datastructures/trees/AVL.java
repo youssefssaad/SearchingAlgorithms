9a1c8d65c330b82c8ed57d8454a8e2defdbd7fe6
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2023-04-07 02:52:43 -0600

    still missing some methods from requirements

--- src/main/java/mylib/datastructures/trees/AVL.java
+++ src/main/java/mylib/datastructures/trees/AVL.java
@@ -1,5 +1,58 @@
 package mylib.datastructures.trees;
 
-public class AVL {
-    
-}
+public class AVL extends BST {
+
+    public AVL() {
+        this.root = null;
+    }
+
+    public AVL(int val) {
+        this.root = new TNode(val);
+    }
+
+    public AVL(TNode obj) {
+        this.root = obj;
+        if (this.root != null) {
+            balance();
+        }
+    }
+
+    private AVL(){}
+
+    public void setRoot(TNode node) {
+        this.root = node;
+        if (this.root != null) {
+            balance();
+        }
+    }
+
+    public TNode getRoot() {
+        return this.root;
+    }
+
+    private void balance(){}
+
+
+
+    @Override
+    public void insert(int val) {
+        super.insert(val);
+        balance();
+    }
+
+    @Override
+    public void insert(TNode node) {
+        super.insert(node);
+        balance();
+    }
+
+    public void delete(int val) {
+        this.root = delete(this.root, val);
+        balance();
+    }
+
+    public TNode Search(int val){}
+
+    public String printInOrder(){}
+
+    public String printBF(){}
