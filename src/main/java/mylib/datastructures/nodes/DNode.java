0c252ddf425385c3e910c49e1b4359774524ebd3
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2023-04-09 17:49:24 -0600

    complete with tests

--- src/main/java/mylib/datastructures/nodes/DNode.java
+++ src/main/java/mylib/datastructures/nodes/DNode.java
@@ -47,131 +47,131 @@ public class Main {
     public static void main(String[] args) {
 
         // DNode object with null data
-        DNode node1 = new DNode(null);
-        assert node1.getData() == null;
+        DNode node = new DNode(null);
+        assert node.getData() == null;
 
         // DNode object with non-null data
-        DNode node2 = new DNode(5);
-        assert node2.getData() == 5;
+        DNode node = new DNode(5);
+        assert node.getData() == 5;
 
         // Setting prev and next to null
-        DNode node3 = new DNode(5);
-        node3.setPrev(null);
-        node3.setNext(null);
-        assert node3.getPrev() == null;
-        assert node3.getNext() == null;
+        DNode node = new DNode(5);
+        node.setPrev(null);
+        node.setNext(null);
+        assert node.getPrev() == null;
+        assert node.getNext() == null;
 
         // Setting prev and next to non-null values
-        DNode node4 = new DNode(5);
-        DNode node5 = new DNode(10);
-        node4.setNext(node5);
-        node5.setPrev(node4);
-        assert node4.getNext() == node5;
-        assert node5.getPrev() == node4;
+        DNode node1 = new DNode(5);
+        DNode node2 = new DNode(10);
+        node1.setNext(node2);
+        node2.setPrev(node1);
+        assert node1.getNext() == node2;
+        assert node2.getPrev() == node1;
 
         // Removing DNode object from DLL
-        DNode node6 = new DNode(5);
-        DNode node7 = new DNode(10);
-        DNode node8 = new DNode(15);
-        node6.setNext(node7);
-        node7.setPrev(node6);
-        node7.setNext(node8);
-        node8.setPrev(node7);
-        node6.setNext(node8);
-        node8.setPrev(node6);
-        node7.setPrev(null);
-        node7.setNext(null);
-        assert node6.getNext() == node8;
-        assert node8.getPrev() == node6;
-        assert node7.getPrev() == null;
-        assert node7.getNext() == null;
+        DNode node1 = new DNode(5);
+        DNode node2 = new DNode(10);
+        DNode node3 = new DNode(15);
+        node1.setNext(node2);
+        node2.setPrev(node1);
+        node2.setNext(node3);
+        node3.setPrev(node2);
+        node1.setNext(node3);
+        node3.setPrev(node1);
+        node2.setPrev(null);
+        node2.setNext(null);
+        assert node1.getNext() == node3;
+        assert node3.getPrev() == node1;
+        assert node2.getPrev() == null;
+        assert node2.getNext() == null;
 
         // Adding DNode object to DLL
-        DNode node9 = new DNode(5);
-        DNode node10 = new DNode(10);
-        DNode node11 = new DNode(15);
-        DNode node12 = new DNode(20);
-        node9.setNext(node10);
-        node10.setPrev(node9);
-        node10.setNext(node11);
-        node11.setPrev(node10);
-        node12.setPrev(node10);
-        node12.setNext(node11);
-        node10.setNext(node12);
-        node11.setPrev(node12);
-        assert node9.getNext() == node10;
-        assert node10.getPrev() == node9;
-        assert node10.getNext() == node12;
-        assert node12.getPrev() == node10;
-        assert node12.getNext() == node11;
-        assert node11.getPrev() == node12;
+        DNode node1 = new DNode(5);
+        DNode node2 = new DNode(10);
+        DNode node3 = new DNode(15);
+        DNode node4 = new DNode(20);
+        node1.setNext(node2);
+        node2.setPrev(node1);
+        node2.setNext(node3);
+        node3.setPrev(node2);
+        node4.setPrev(node2);
+        node4.setNext(node3);
+        node2.setNext(node4);
+        node3.setPrev(node4);
+        assert node1.getNext() == node2;
+        assert node2.getPrev() == node1;
+        assert node2.getNext() == node4;
+        assert node4.getPrev() == node2;
+        assert node4.getNext() == node3;
+        assert node3.getPrev() == node4;
 
         // `getData()` to retrieve data of `DNode` object
-        DNode node13 = new DNode(5);
-        assert node13.getData() == 5;
+        DNode node = new DNode(5);
+        assert node.getData() == 5;
 
         // `setData()` to set data of `DNode` object
-        DNode node14 = new DNode(5);
-        node14.setData(10);
-        assert node14.getData() == 10;
+        DNode node = new DNode(5);
+        node.setData(10);
+        assert node.getData() == 10;
 
         // `getPrev()` to retrieve previous `DNode` object in DLL
-        DNode node15 = new DNode(5);
-        DNode node16 = new DNode(10);
-        DNode node17 = new DNode(15);
-        node15.setNext(node16);
-        node16.setPrev(node15);
-        node16.setNext(node17);
-        node17.setPrev(node16);
-        assert node16.getPrev() == node15;
+        DNode node1 = new DNode(5);
+        DNode node2 = new DNode(10);
+        DNode node3 = new DNode(15);
+        node1.setNext(node2);
+        node2.setPrev(node1);
+        node2.setNext(node3);
+        node3.setPrev(node2);
+        assert node2.getPrev() == node1;
 
         // `setPrev()` to set previous `DNode` object in DLL
-        DNode node17 = new DNode(5);
-        DNode node18 = new DNode(10);
-        DNode node19 = new DNode(15);
-        node17.setNext(node18);
-        node18.setPrev(node17);
-        node18.setNext(node19);
-        node19.setPrev(node18);
-        DNode node20 = new DNode(20);
-        node20.setPrev(node18);
-        node18.setPrev(node20);
-        node20.setNext(node18);
-        node18.setNext(node19);
-        assert node20.getNext() == node18;
-        assert node18.getPrev() == node20;
-        assert node18.getNext() == node19;
-        assert node19.getPrev() == node18;
+        DNode node1 = new DNode(5);
+        DNode node2 = new DNode(10);
+        DNode node3 = new DNode(15);
+        node1.setNext(node2);
+        node2.setPrev(node1);
+        node2.setNext(node3);
+        node3.setPrev(node2);
+        DNode node4 = new DNode(20);
+        node4.setPrev(node2);
+        node2.setPrev(node4);
+        node4.setNext(node2);
+        node2.setNext(node3);
+        assert node4.getNext() == node2;
+        assert node2.getPrev() == node4;
+        assert node2.getNext() == node3;
+        assert node3.getPrev() == node2;
 
         // `getNext()` to retrieve `DNode` object in DLL
-        DNode node21 = new DNode(5);
-        DNode node22 = new DNode(10);
-        DNode node23 = new DNode(15);
-        node21.setNext(node22);
-        node22.setPrev(node21);
-        node22.setNext(node23);
-        node23.setPrev(node22);
-        assert node22.getNext() == node23;
+        DNode node1 = new DNode(5);
+        DNode node2 = new DNode(10);
+        DNode node3 = new DNode(15);
+        node1.setNext(node2);
+        node2.setPrev(node1);
+        node2.setNext(node3);
+        node3.setPrev(node2);
+        assert node2.getNext() == node3;
 
         // `setNext()` to set next `DNode` object in DLL
-        DNode node24 = new DNode(5);
-        DNode node25 = new DNode(10);
-        DNode node26 = new DNode(15);
-        node24.setNext(node25);
-        node25.setPrev(node24);
-        node25.setNext(node26);
-        node26.setPrev(node25);
-        DNode node27 = new DNode(20);
-        node27.setPrev(node25);
-        node25.setNext(node27);
-        node27.setNext(node26);
-        node26.setPrev(node27);
-        assert node24.getNext() == node25;
-        assert node25.getPrev() == node24;
-        assert node25.getNext() == node27;
-        assert node27.getPrev() == node25;
-        assert node27.getNext() == node26;
-        assert node26.getPrev() == node27;
+        DNode node1 = new DNode(5);
+        DNode node2 = new DNode(10);
+        DNode node3 = new DNode(15);
+        node1.setNext(node2);
+        node2.setPrev(node1);
+        node2.setNext(node3);
+        node3.setPrev(node2);
+        DNode node4 = new DNode(20);
+        node4.setPrev(node2);
+        node2.setNext(node4);
+        node4.setNext(node3);
+        node3.setPrev(node4);
+        assert node1.getNext() == node2;
+        assert node2.getPrev() == node1;
+        assert node2.getNext() == node4;
+        assert node4.getPrev() == node2;
+        assert node4.getNext() == node3;
+        assert node3.getPrev() == node4;
 
     }
 }
--- src/main/java/mylib/datastructures/nodes/SNode.java
+++ src/main/java/mylib/datastructures/nodes/SNode.java
@@ -35,78 +35,78 @@ public class Main {
     public static void main(String[] args) {
 
         // SNode object with null data
-        SNode node1 = new SNode(null);
-        assert node1.getData() == null;
+        SNode node = new SNode(null);
+        assert node.getData() == null;
 
         // SNode object with non-null data
-        SNode node2 = new SNode(5);
-        assert node2.getData() == 5;
+        SNode node = new SNode(5);
+        assert node.getData() == 5;
 
         // Setting next to null
-        SNode node3 = new SNode(5);
-        node3.setNext(null);
-        assert node3.getNext() == null;
+        SNode node = new SNode(5);
+        node.setNext(null);
+        assert node.getNext() == null;
 
         // Setting next to non-null values
-        SNode node4 = new SNode(5);
-        SNode node5 = new SNode(10);
-        node4.setNext(node5);
-        assert node4.getNext() == node5;
+        SNode node1 = new SNode(5);
+        SNode node2 = new SNode(10);
+        node1.setNext(node2);
+        assert node1.getNext() == node2;
 
         // Removing SNode object from SLL
-        SNode node6 = new SNode(5);
-        SNode node7 = new SNode(10);
-        SNode node8 = new SNode(15);
-        node6.setNext(node7);
-        node7.setNext(node8);
-        node6.setNext(node8);
-        node7.setNext(null);
-        assert node6.getNext() == node8;
-        assert node7.getNext() == null;
+        SNode node1 = new SNode(5);
+        SNode node2 = new SNode(10);
+        SNode node3 = new SNode(15);
+        node1.setNext(node2);
+        node2.setNext(node3);
+        node1.setNext(node3);
+        node2.setNext(null);
+        assert node1.getNext() == node3;
+        assert node2.getNext() == null;
 
         // Adding SNode object to SLL
-        SNode node9 = new SNode(5);
-        SNode node10 = new SNode(10);
-        SNode node11 = new SNode(15);
-        SNode node12 = new SNode(20);
-        node9.setNext(node10);
-        node10.setNext(node11);
-        node12.setNext(node11);
-        node10.setNext(node12);
-        assert node9.getNext() == node10;
-        assert node10.getNext() == node12;
-        assert node12.getNext() == node11;
+        SNode node1 = new SNode(5);
+        SNode node2 = new SNode(10);
+        SNode node3 = new SNode(15);
+        SNode node4 = new SNode(20);
+        node1.setNext(node2);
+        node2.setNext(node3);
+        node3.setNext(node3);
+        node4.setNext(node4);
+        assert node1.getNext() == node2;
+        assert node2.getNext() == node4;
+        assert node4.getNext() == node3;
 
         // `getData()` to retrieve data of `SNode` object
-        SNode node13 = new SNode(5);
-        assert node13.getData() == 5;
+        SNode node = new SNode(5);
+        assert node.getData() == 5;
 
         // `setData()` to set data of `SNode` object
-        SNode node14 = new SNode(5);
-        node14.setData(10);
-        assert node14.getData() == 10;
+        SNode node = new SNode(5);
+        node.setData(10);
+        assert node.getData() == 10;
 
         // `getNext()` to retrieve `SNode` object in SLL
-        SNode node15 = new SNode(5);
-        SNode node16 = new SNode(10);
-        SNode node17 = new SNode(15);
-        node15.setNext(node16);
-        node16.setNext(node17);
-        assert node15.getNext() == node16;
-        assert node16.getNext() == node17;
+        SNode node1 = new SNode(5);
+        SNode node2 = new SNode(10);
+        SNode node3 = new SNode(15);
+        node1.setNext(node2);
+        node2.setNext(node3);
+        assert node1.getNext() == node2;
+        assert node2.getNext() == node3;
 
         // `setNext()` to set next `SNode` object in SLL
-        SNode node18 = new SNode(5);
-        SNode node19 = new SNode(10);
-        SNode node20 = new SNode(15);
-        node18.setNext(node19);
-        node19.setNext(node20);
-        SNode node21 = new SNode(20);
-        node19.setNext(node21);
-        node21.setNext(node20);
-        assert node18.getNext() == node19;
-        assert node19.getNext() == node21;
-        assert node21.getNext() == node20;
+        SNode node1 = new SNode(5);
+        SNode node2 = new SNode(10);
+        SNode node3 = new SNode(15);
+        node1.setNext(node2);
+        node2.setNext(node3);
+        SNode node4 = new SNode(20);
+        node2.setNext(node4);
+        node4.setNext(node3);
+        assert node1.getNext() == node2;
+        assert node2.getNext() == node4;
+        assert node4.getNext() == node3;
     }
 }
 
