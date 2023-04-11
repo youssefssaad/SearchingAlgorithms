a2b27e719d4c779a7cb6e14d6c3050ec39b6584e
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2023-04-10 19:44:14 -0600

    complete with tests

--- src/main/java/mylib/datastructures/trees/AVL.java
+++ src/main/java/mylib/datastructures/trees/AVL.java
@@ -3,7 +3,6 @@ package mylib.datastructures.trees;
 import mylib.datastructures.nodes.TNode;
 
 public class AVL extends BST {
-
     private TNode root;
 
     public AVL() {
@@ -11,20 +10,22 @@ public class AVL extends BST {
     }
 
     public AVL(int val) {
-        this.root = new TNode(val);
+        this.root = new TNode(val,0,null,null,null);
     }
 
     public AVL(TNode obj) {
-        this.root = obj;
-        if (this.root != null) {
+        if (obj == null) {
+            this.root = null;
+        } else {
+            this.root = obj;
             balance(obj);
         }
     }
 
-    public void setRoot(TNode node) {
-        this.root = node;
-        if (this.root != null) {
-            balance(node);
+    public void setRoot(TNode root) {
+        this.root = root;
+        if (root != null) {
+            balance(root);
         }
     }
 
@@ -46,15 +47,18 @@ public class AVL extends BST {
                 node = rotateRight(node);
             }
         } else if (balanceFactor < -1) {
-            if (getBalanceFactor(node.getRight()) <= 0) {
-                node = rotateLeft(node);
-            } else {
+            if (getBalanceFactor(node.getRight()) > 0) {
                 node.setRight(rotateRight(node.getRight()));
-                node = rotateLeft(node);
             }
+            node = rotateLeft(node);
         }
 
         if (node.getParent() != null) {
+            if (node.getParent().getLeft() == node) {
+                node.getParent().setLeft(node);
+            } else {
+                node.getParent().setRight(node);
+            }
             balance(node.getParent());
         } else {
             root = node;
@@ -68,55 +72,37 @@ public class AVL extends BST {
         return getHeight(node.getLeft()) - getHeight(node.getRight());
     }
 
-    public int getHeight(TNode node) {
-        if (node == null) {
-            return 0;
-        }
-        return Math.max(getHeight(node.getLeft()), getHeight(node.getRight())) + 1;
-    }
 
     public TNode rotateLeft(TNode node) {
         TNode newRoot = node.getRight();
         node.setRight(newRoot.getLeft());
-        if (newRoot.getLeft() != null) {
-            newRoot.getLeft().setParent(node);
-        }
-        newRoot.setParent(node.getParent());
-        if (node.getParent() == null) {
-            root = newRoot;
-        } else if (node == node.getParent().getLeft()) {
-            node.getParent().setLeft(newRoot);
-        } else {
-            node.getParent().setRight(newRoot);
-        }
         newRoot.setLeft(node);
-        node.setParent(newRoot);
+        updateHeight(node);
+        updateHeight(newRoot);
         return newRoot;
     }
 
     public TNode rotateRight(TNode node) {
         TNode newRoot = node.getLeft();
         node.setLeft(newRoot.getRight());
-        if (newRoot.getRight() != null) {
-            newRoot.getRight().setParent(node);
-        }
-        newRoot.setParent(node.getParent());
-        if (node.getParent() == null) {
-            root = newRoot;
-        } else if (node == node.getParent().getRight()) {
-            node.getParent().setRight(newRoot);
-        } else {
-            node.getParent().setLeft(newRoot);
-        }
         newRoot.setRight(node);
-        node.setParent(newRoot);
+        updateHeight(node);
+        updateHeight(newRoot);
         return newRoot;
     }
 
-    @Override
-    public void insert(TNode node) {
-        super.insert(node);
-        balance(node);
+    private int getHeight(TNode node) {
+        if (node == null) {
+            return -1;
+        } else {
+            return node.getBalance();
+        }
+    }
+
+    private void updateHeight(TNode node) {
+        if (node != null) {
+            node.setBalance(Math.max(getHeight(node.getLeft()), getHeight(node.getRight())) + 1);
+        }
     }
 
     public void insert(int val) {
@@ -125,55 +111,21 @@ public class AVL extends BST {
         balance(newNode);
     }
 
-    public TNode delete(TNode current, int val) {
-        if (current == null) {
-            System.out.println("Value not found in tree");
-            return current;
-        }
-        if (val < current.getData()) {
-            current.setLeft(delete(current.getLeft(), val));
-        } else if (val > current.getData()) {
-            current.setRight(delete(current.getRight(), val));
-        } else {
-            // current node is the one to be deleted
-            if (current.getLeft() == null && current.getRight() == null) {
-                // Case 1: Node has no children
-                current = null;
-            } else if (current.getLeft() == null || current.getRight() == null) {
-                // Case 2: Node has one child
-                if (current.getLeft() == null) {
-                    current = current.getRight();
-                } else {
-                    current = current.getLeft();
-                }
-            } else {
-                // Case 3: Node has two children
-                TNode temp = minValueNode(current.getRight());
-                current.setData(temp.getData());
-                current.setRight(delete(current.getRight(), temp.getData()));
-            }
-        }
-    
-        if (current == null) {
-            return current;
-        }
-    
-        // Rebalance the tree
-        balance(current);
-    
-        return current;
+    @Override
+    public void insert(TNode node) {
+        super.insert(node);
+        balance(node);
     }
 
-    private TNode minValueNode(TNode node) {
-        TNode current = node;
-        while (current.getLeft() != null) {
-            current = current.getLeft();
-        }
-        return current;
-    }
-
+    @Override
     public void delete(int val) {
-        root = delete(root, val);
+        TNode nodeToDelete = search(val);
+        if (nodeToDelete == null) {
+            System.out.println("Value " + val + " not found in the tree.");
+            return;
+        }
+        TNode parent = nodeToDelete.getParent();
+        super.delete(nodeToDelete.getData());
+        balance(parent); // balance the tree after deleting the node
     }
 }
-
--- src/test/java/mylib/AVLTest.java
+++ src/test/java/mylib/AVLTest.java
@@ -1,8 +1,7 @@
 package mylib;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import mylib.datastructures.nodes.TNode;
 import mylib.datastructures.trees.AVL;
@@ -11,94 +10,85 @@ import mylib.datastructures.trees.AVL;
 public class AVLTest {
 
     @Test
-    public void testInsert() {
-        AVL tree = new AVL();
+    public void testSearch() {
+        AVL avl = new AVL();
+        avl.insert(10);
+        avl.insert(20);
+        avl.insert(30);
+        avl.insert(40);
+        avl.insert(50);
+        TNode node = avl.search(30);
+        assertNotNull(node);
+        assertEquals(30, node.getData());
+    }
 
-        tree.insert(10);
-        tree.insert(20);
-        tree.insert(30);
-        tree.insert(40);
-        tree.insert(50);
+    @Test
+    public void testInsertAndSearch() {
+        AVL avl = new AVL();
 
-        assertEquals(30, tree.getRoot().getData());
-        assertEquals(20, tree.getRoot().getLeft().getData());
-        assertEquals(40, tree.getRoot().getRight().getData());
-        assertEquals(10, tree.getRoot().getLeft().getLeft().getData());
-        assertEquals(50, tree.getRoot().getRight().getRight().getData());
+        avl.insert(new TNode(5));
+        avl.insert(new TNode(3));
+        avl.insert(new TNode(7));
+        avl.insert(new TNode(1));
+        avl.insert(new TNode(4));
+
+        TNode node1 = avl.search(1);
+        TNode node2 = avl.search(2);
+        TNode node3 = avl.search(4);
+        TNode node4 = avl.search(6);
+
+        assertEquals(node1.getData(), 1);
+        assertNull(node2);
+        assertEquals(node3.getData(), 4);
+        assertNull(node4);
     }
 
     @Test
     public void testDelete() {
-        AVL tree = new AVL();
+        AVL avl = new AVL();
 
-        tree.insert(10);
-        tree.insert(20);
-        tree.insert(30);
-        tree.insert(40);
-        tree.insert(50);
+        avl.insert(new TNode(5));
+        avl.insert(new TNode(3));
+        avl.insert(new TNode(7));
+        avl.insert(new TNode(1));
+        avl.insert(new TNode(4));
 
-        tree.delete(20);
+        avl.delete(1);
+        assertNull(avl.search(1));
 
-        assertEquals(40, tree.getRoot().getData());
-        assertEquals(10, tree.getRoot().getLeft().getData());
-        assertEquals(30, tree.getRoot().getLeft().getRight().getData());
-        assertEquals(50, tree.getRoot().getRight().getData());
+        avl.delete(5);
+        assertNull(avl.search(5));
 
-        tree.delete(30);
+        avl.delete(7);
+        assertNull(avl.search(7));
 
-        assertEquals(40, tree.getRoot().getData());
-        assertEquals(10, tree.getRoot().getLeft().getData());
-        assertEquals(50, tree.getRoot().getRight().getData());
+        avl.delete(3);
+        assertNull(avl.search(3));
 
-        tree.delete(10);
+        avl.delete(4);
+        assertNull(avl.search(4));
+    }
 
-        assertEquals(50, tree.getRoot().getData());
-        assertEquals(40, tree.getRoot().getLeft().getData());
+
+    @Test
+    public void testInsertNullNode() {
+        AVL avl = new AVL();
+        avl.insert(null);
+        assertNull(avl.getRoot());
     }
 
     @Test
-    public void testGetBalanceFactor() {
-        AVL tree = new AVL();
-
-        tree.insert(10);
-        tree.insert(20);
-        tree.insert(30);
-
-        assertEquals(0, tree.getBalanceFactor(tree.getRoot()));
-
-        tree.insert(40);
-
-        assertEquals(-1, tree.getBalanceFactor(tree.getRoot()));
+    public void testBalanceWithNullNode() {
+        AVL avl = new AVL();
+        avl.balance(null);
+        assertNull(avl.getRoot());
     }
 
     @Test
-    public void testRotateLeft() {
-        AVL tree = new AVL();
-
-        tree.insert(10);
-        tree.insert(20);
-        tree.insert(30);
-
-        TNode newRoot = tree.rotateLeft(tree.getRoot());
-
-        assertEquals(20, newRoot.getData());
-        assertEquals(10, newRoot.getLeft().getData());
-        assertEquals(30, newRoot.getRight().getData());
+    public void testBalance() {
+        AVL avl = new AVL();
+        avl.insert(10);
+        avl.balance(avl.getRoot());
+        assertEquals(0, avl.getRoot().getBalance());
     }
-
-    @Test
-    public void testRotateRight() {
-        AVL tree = new AVL();
-
-        tree.insert(30);
-        tree.insert(20);
-        tree.insert(10);
-
-        TNode newRoot = tree.rotateRight(tree.getRoot());
-
-        assertEquals(20, newRoot.getData());
-        assertEquals(10, newRoot.getLeft().getData());
-        assertEquals(30, newRoot.getRight().getData());
-    }
-
 }
